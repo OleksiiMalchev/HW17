2a65f7155e3ab315ed8da62fb5b7c5a0f6410e02
OleksiiMalchev <oleksiimalchev@gmail.com>
2023-01-28 14:30:56 +0200

    HW 17 is done

---
+++ src/main/java/org/example/ServletHelloWorld.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ServletHelloWorld extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("<h2>Hello World!</h2>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
